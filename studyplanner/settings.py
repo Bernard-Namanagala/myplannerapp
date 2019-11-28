9692e9fea1a223d60eca259429e3f0e2acbead21
Bernard Namangala <namangalabernard@gmail.com>
2019-11-28 17:58:19 +0200

    "added allowed hosts"

--- studyplanner/settings.py
+++ studyplanner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '810gffrke!un!+#hj59!eslm!@ds+=i7^m0g07qju$h_f^f_rq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.43.200', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition

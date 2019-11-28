9767d83864919922404a20c1ab9abfe4e96070a9
Bernard Namangala <namangalabernard@gmail.com>
2019-11-28 17:53:59 +0200

    "debug set to false"

--- studyplanner/settings.py
+++ studyplanner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '810gffrke!un!+#hj59!eslm!@ds+=i7^m0g07qju$h_f^f_rq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.43.200', 'localhost', '127.0.0.1']
 
@@ -117,7 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
